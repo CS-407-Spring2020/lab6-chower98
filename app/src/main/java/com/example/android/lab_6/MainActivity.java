31cd48b4c1662b86403af50282ad0d5162d3387c
chower98 <56125443+chower98@users.noreply.github.com>
2020-03-05 21:59:05 -0600

    Lab 6 milestone 1

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.android.lab_6;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.FragmentActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity {
+
+    private final LatLng mDestinationLatLng = new LatLng(43.075384, -89.404258);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+
+            googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng)
+                    .title("Destination"));
+        });
+//        mapFragment.getMapAsync(googleMap -> {
+//            mMap = googleMap;
+//            //code to display marker
+//            googleMap.addMarker(new MarkerOptions()
+//                    .position(mDestinationLatLng).title("Destination"));
+//        });
     }
 }
